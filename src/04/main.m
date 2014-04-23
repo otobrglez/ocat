dccaae7093a0172ba17fe17fda66c5754b59e83f
Oto Brglez <otobrglez@gmail.com>
2014-04-23 17:04:55 +0100

    Adding predefined macros.

--- src/04/main.m
+++ src/04/main.m
@@ -25,6 +25,12 @@ int main(int argc, const char * argv[]){
 
   NSLog(@"Reverse %@", [myString reverseString]);
 
+  NSLog(@"File :%s\n", __FILE__ );
+  NSLog(@"Date :%s\n", __DATE__ );
+  NSLog(@"Time :%s\n", __TIME__ );
+  NSLog(@"Line :%d\n", __LINE__ );
+  NSLog(@"ANSI :%d\n", __STDC__ );
+
   [pool drain];
   return EXIT_SUCCESS;
 }
